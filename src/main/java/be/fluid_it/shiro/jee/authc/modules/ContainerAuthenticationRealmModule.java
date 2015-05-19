8f4b8d586831d6676e8f457fc4cefa6342adcb6b
rvs-fluid-it <koen.roevens@gmail.com>
2015-05-19 17:01:22 +0200

    Added Guice wiring

--- src/main/java/be/fluid_it/shiro/jee/authc/modules/ContainerAuthenticationRealmModule.java
+++ src/main/java/be/fluid_it/shiro/jee/authc/modules/ContainerAuthenticationRealmModule.java
@@ -1,5 +1,7 @@
 package be.fluid_it.shiro.jee.authc.modules;
 
+import be.fluid_it.guice.extensions.multibindings.shiro.realms.Deployment;
+import be.fluid_it.guice.extensions.multibindings.shiro.realms.DeploymentSimpleFacet;
 import be.fluid_it.guice.extensions.multibindings.shiro.realms.RealmKey;
 import be.fluid_it.guice.extensions.multibindings.shiro.realms.modules.MultiRealmBinder;
 import be.fluid_it.shiro.jee.authc.ContainerAuthenticationRealm;
@@ -10,7 +12,7 @@ public class ContainerAuthenticationRealmModule implements Module {
     @Override
     public void configure(Binder binder) {
         MultiRealmBinder.newMultiRealmBinder(binder)
-                .addBinding(new RealmKey(ContainerAuthenticationRealm.class))
+                .addBinding(new RealmKey(ContainerAuthenticationRealm.class).cut(new DeploymentSimpleFacet(Deployment.Option.WAR)))
                 .to(ContainerAuthenticationRealm.class);
     }
 }
