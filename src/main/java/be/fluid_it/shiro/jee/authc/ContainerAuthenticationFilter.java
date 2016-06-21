c1656bcab51be6205e67b69c4611dc3f5f84754c
rvs-fluid-it <koen.roevens@gmail.com>
2016-06-21 14:29:17 +0200

    Only propagate JEE principal to subject if authenticated.

--- src/main/java/be/fluid_it/shiro/jee/authc/ContainerAuthenticationFilter.java
+++ src/main/java/be/fluid_it/shiro/jee/authc/ContainerAuthenticationFilter.java
@@ -3,31 +3,46 @@ package be.fluid_it.shiro.jee.authc;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterConfig;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.ServletContext;
+import javax.servlet.DispatcherType;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+
 import java.io.IOException;
 import java.util.EnumSet;
 
 public class ContainerAuthenticationFilter implements Filter {
     private static final Logger logger = LoggerFactory.getLogger(ContainerAuthenticationRealm.class);
-    private ContainerAuthenticationBridge containerAutcBridge;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        this.containerAutcBridge = new ContainerAuthenticationBridge();
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        logger.trace("Synchronize the Shiro subject with the JEE principal ...");
-        String message = containerAutcBridge.synchronizeShiroSubjectWithJEEPrincipal(request);
-        if (message != null) {
-            logger.info(message);
+        if (isAuthenticated(request)) {
+            logger.trace("Synchronize the Shiro subject with the JEE principal ...");
+            String message = new ContainerAuthenticationBridge().synchronizeShiroSubjectWithJEEPrincipal(request);
+            if (message != null) {
+                logger.info(message);
+            }
         }
         chain.doFilter(request, response);
     }
 
     @Override
     public void destroy() {
+    }
+
+    private boolean isAuthenticated(ServletRequest request) {
+        return request instanceof HttpServletRequest && ((HttpServletRequest)request).getUserPrincipal() != null;
     }
 
     public static void registerFilter(ServletContext servletContext) {
